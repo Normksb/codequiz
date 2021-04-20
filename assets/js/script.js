9fe7fb870b2e67817572ac1c1ff8977fa8308679
Norman Bernard <nksb414@gmail.com>
2021-04-20 10:35:46 +0800

    added javascript question array of object

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,39 @@ var highScoresSection = document.getElementById("highscores")
 
 var secondsLeft = 75;
 var currentScore = 0;
+
+var questions = [
+    {
+        question: "Javascript first appeared in the year...",
+        answer1: "1995",
+        answer2: "2005",
+        answer3: "2015",
+        answer4: "2020",
+        correctAnswer: "1"
+    },
+    {
+        question: "The N in JSON stands for...",
+        answer1: "Node",
+        answer2: "Notation",
+        answer3: "Number",
+        answer4: "Niche",
+        correctAnswer: "2"
+    },
+    {
+        question: "Which of the following is not an arithmetic operator?",
+        answer1: "+",
+        answer2: "-",
+        answer3: "&",
+        answer4: "%",
+        correctAnswer: "3"
+    },
+    {
+        question: "Which of the following is not a primitive data type?",
+        answer1: "undefined",
+        answer2: "boolean",
+        answer3: "number",
+        answer4: "object",
+        correctAnswer: "4"
+    }    
+    
+]
