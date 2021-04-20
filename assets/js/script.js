fe2255936948a0a5b2ed21bce7429ad26f0fbf7c
Norman Bernard <nksb414@gmail.com>
2021-04-20 09:42:58 +0800

    added all required vars to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+var viewHighScoresButton = document.getElementById("view-highscores");
+var timeLeft = document.getElementById("time-left");
+var quizIntroTextSection = document.getElementById("quiz-intro-text");
+var startQuizButton = document.getElementById("start-quiz");
+var theQuizSection = document.getElementById("the-quiz");
+var answerButton1 = document.getElementById("answer-1");
+var answerButton2 = document.getElementById("answer-2");
+var answerButton3 = document.getElementById("answer-3");
+var answerButton4 = document.getElementById("answer-4");
+var highScoresSection = document.getElementById("highscores")
+
+var secondsLeft = 75;
+var currentScore = 0;
